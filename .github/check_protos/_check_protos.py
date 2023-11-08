158a0ce6c1c38b9b814db10cae71ae9f9d17bbc5
Joel Dixon <joel.dixon@ni.com>
2023-11-08 16:50:29 -0600

    Fix path errors so the only missing .proto is session.proto

--- .github/check_protos/_check_protos.py
+++ .github/check_protos/_check_protos.py
@@ -4,19 +4,22 @@
 import pathlib
 import sys
 import grpc_tools.protoc
+import pkg_resources
 
-PROTO_PATH = pathlib.Path(__file__).parent.parent.parent / "ni"
+PROTO_ROOT_PATH = pathlib.Path(__file__).parent.parent.parent
+PROTO_PATH = PROTO_ROOT_PATH / "ni"
 PROTO_FILES = list(PROTO_PATH.rglob("*.proto"))
 
 # Generate python files from .proto files with protoc.
 arguments = [
     "protoc",
-    f"--proto_path={str(PROTO_PATH)}",
+    f"--proto_path={str(PROTO_ROOT_PATH)}",
+    f"--proto_path={pkg_resources.resource_filename('grpc_tools', '_proto')}",
     f"--python_out={str(PROTO_PATH)}",
     f"--grpc_python_out={str(PROTO_PATH)}",
 ]
 
-arguments += [str(path.relative_to(PROTO_PATH)).replace("\\", "/") for path in PROTO_FILES]
+arguments += [str(path.relative_to(PROTO_ROOT_PATH)).replace("\\", "/") for path in PROTO_FILES]
 
 results = grpc_tools.protoc.main(arguments)
 if results != 0:
