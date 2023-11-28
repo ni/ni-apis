cfdfc7466868d255a49e6970083f83153273a0ce
Joel Dixon <38357562+dixonjoel@users.noreply.github.com>
2023-11-28 16:01:08 -0600

    Add xydata.proto under ni/protobuf/types (#6)
    * Add xydata.proto
    
    * Add .proto files under ni/protobuf to be generated in check_protos
    
    * check_protos: Include all proto files and handle only the grpc_device protos specially
    
    * check_protos: Use correct output path
    
    * check_protos: Delete unused var
    
    ---------
    
    Co-authored-by: Brad Keryan <brad.keryan@ni.com>

--- .github/check_protos/_check_protos.py
+++ .github/check_protos/_check_protos.py
@@ -7,9 +7,11 @@ import grpc_tools.protoc
 import pkg_resources
 
 PROTO_ROOT_PATH = pathlib.Path(__file__).parent.parent.parent
-PROTO_PATH = PROTO_ROOT_PATH / "ni" / "measurementlink"
-PROTO_FILES = list(PROTO_PATH.rglob("*.proto"))
+PROTO_PATH = PROTO_ROOT_PATH / "ni"
 GRPC_DEVICE_PROTO_PATH = PROTO_ROOT_PATH / "ni" / "grpcdevice" / "v1"
+
+PROTO_FILES = list(path for path in PROTO_PATH.rglob("*.proto") if not path.is_relative_to(GRPC_DEVICE_PROTO_PATH))
+GRPC_DEVICE_PROTO_FILES = list(GRPC_DEVICE_PROTO_PATH.rglob("*.proto"))
 
 def main():
     # Generate python files from .proto files with protoc.
@@ -18,16 +20,18 @@ def main():
         f"--proto_path={str(PROTO_ROOT_PATH)}",
         f"--proto_path={str(GRPC_DEVICE_PROTO_PATH)}",
         f"--proto_path={pkg_resources.resource_filename('grpc_tools', '_proto')}",
-        f"--python_out={str(PROTO_PATH)}",
-        f"--grpc_python_out={str(PROTO_PATH)}",
+        f"--python_out={str(PROTO_ROOT_PATH)}",
+        f"--grpc_python_out={str(PROTO_ROOT_PATH)}",
     ]
 
     arguments += [str(path.relative_to(PROTO_ROOT_PATH)).replace("\\", "/") for path in PROTO_FILES]
+    arguments += [str(path.relative_to(GRPC_DEVICE_PROTO_PATH)).replace("\\", "/") for path in GRPC_DEVICE_PROTO_FILES]
+
     print("::group::Arguments for protoc")
     for argument in arguments:
         print(argument)
     print("::endgroup::")
-    
+
     result = grpc_tools.protoc.main(arguments)
     if result != 0:
         sys.exit(result)
